b3a0dfbe547d0238bb1023e7a06eb7d5c5a1c0d8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-12 03:35:30 +0600

    navigate lofin page from orders page if token have any problem or wrong token

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
+    const navigate = useNavigate()
+    const [allProducts, setAllProducts] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/orders', {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => {
+            if(res.status === 401 || res.status === 403){
+                navigate('/login')
+            }
+            // console.log(res)
+           return res.json()
+        })
+        .then(data => setAllProducts(data))
+    },[])
     return (
         <div>
             <Container>
-                <h2>Orders</h2>
+                <h2>Orders {allProducts.length}</h2>
             </Container>
             
         </div>
