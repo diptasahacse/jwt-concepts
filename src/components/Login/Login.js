36f48b4eef7261994c93e91811cb84fdc58ca956
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-12 02:07:03 +0600

    generate token for authentic use and store it in localstorate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate()
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const loginOnSubmitHandler = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const pass = passwordRef.current.value;
+        // console.log(email, pass)
+        const user = {
+            email,
+            pass
+        }
+
+        fetch('http://localhost:5000/login', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.success){
+                    localStorage.setItem('accessToken', data.accessToken)
+                    navigate('/orders')
+                }
+                else{
+                    console.log(data.success)
+
+                }
+
+            })
+
+    }
     return (
         <div>
             <Container>
                 <h2>Login</h2>
                 <div className='py-4'>
-                    <Form>
+                    <Form onSubmit={loginOnSubmitHandler}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control required ref={emailRef} type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control required ref={passwordRef} type="password" placeholder="Password" />
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
